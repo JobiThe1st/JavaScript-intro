191a170c48a0a3592b2c3d2add010128dd51f44b
JobiThe1st <104764297+JobiThe1st@users.noreply.github.com>
2022-06-24 11:31:51 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,7 @@
+const nameOfStudent = "Jesutobi";
+let heightOfStudent = "175cm";
+let countryOfStudent = "Nigeria";
+
+console.log(nameOfStudent);
+console.log(heightOfStudent);
+console.log(countryOfStudent);
