2668bb98340c228b668aee3498aaf13d96151e1f
JobiThe1st <104764297+JobiThe1st@users.noreply.github.com>
2022-06-24 12:49:57 +0100

    Delete intro.js

--- intro.js
+++
@@ -1,7 +0,0 @@
-const nameOfStudent = "Jesutobi";
-let heightOfStudent = "175cm";
-let countryOfStudent = "Nigeria";
-
-console.log(nameOfStudent);
-console.log(heightOfStudent);
-console.log(countryOfStudent);
